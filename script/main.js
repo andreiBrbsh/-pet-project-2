3fad04d84b8aaca6fe2246bc5625594ed076f916
andreiBrbsh <andrei.brbsh@icloud.com>
2023-01-12 19:50:44 +0200

    header + menu

--- script/main.js
+++ script/main.js
@@ -1,2 +1,16 @@
 'use strict';
 
+
+
+
+// burger 
+const container = document.querySelector('#header-container');
+document.addEventListener('click', (e) => {
+        const burger = document.querySelector('.header__logo-burger');
+        const menuBody = document.querySelector('.menu__list');
+        if(e.composedPath().includes(burger)) {
+                container.classList.toggle('active');
+        } else if (!e.composedPath().includes(burger) && !e.composedPath().includes(menuBody)) {
+                container.classList.remove('active');
+        }
+})
